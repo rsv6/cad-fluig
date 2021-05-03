d9f289db4f8e343753739cfdc0f9cca6b9ff965e
Rafael Santos Vieira <rafaelsvbr@gmail.com>
2021-05-02 22:38:28 -0300

    update: routes files

--- src/routes/delete.js
+++ src/routes/delete.js
@@ -1,6 +1,6 @@
 require('dotenv/config')
 const router = require('express').Router()
 
-router.delete('/api/delete/:id', require(process.env.PTH_CTRL+'selectAllController.js').delete)
+router.delete('/api/delete', require(process.env.PTH_CTRL+'selectAllController.js').delete)
 
 module.exports = router
--- src/routes/selectAll.js
+++ src/routes/selectAll.js
@@ -1,6 +1,6 @@
 require('dotenv/config')
 const router = require('express').Router()
 
-router.get('/api/select', require(process.env.PTH_CTRL+'selectAllController.js').get)
+router.get('/api/select-all', require(process.env.PTH_CTRL+'selectAllController.js').get)
 
 module.exports = router
